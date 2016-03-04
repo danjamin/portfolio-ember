5752941adba5fa315a0ae5d7afde1503763b4826
danjamin <e051ba3be@gmail.com>
2016-03-03 20:58:24 -0800

    CDN, favicon, and updated index.html with description and title

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      prepend: 'http://d14bg7m33q3rnp.cloudfront.net/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
