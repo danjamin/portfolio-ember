026e08b35507620e4edee9ba7e241777f0e3bac7
danjamin <e051ba3be@gmail.com>
2016-03-04 19:42:11 -0800

    Initial experience page

---
+++ app/pods/experience/controller.js
@@ -0,0 +1,3 @@
+export default Ember.Controller.extend({
+  chartColor: "#1758A7"
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('experience', { path: '/experience' });
 });
 
 export default Router;
